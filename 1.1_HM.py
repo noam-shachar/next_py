3869b23069d7678f88e28025c51b9c349318a532
noam-shachar <128282432+noam-shachar@users.noreply.github.com>
2023-05-29 15:02:32 +0300

    Delete 1.1_HM.py

--- 1.1_HM.py
+++
@@ -1,6 +0,0 @@
-def double_letter(my_str):
-    return my_str + my_str
-def main():
-    if __name__ == '__main__':
-        print("".join(list(map(double_letter, "we are the champions!"))))
-main()

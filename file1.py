9fa516628532cb18cbc75b953c70135df4bf5f50
noam-shachar <128282432+noam-shachar@users.noreply.github.com>
2023-05-29 15:01:17 +0300

    Delete file1.py

--- file1.py
+++
@@ -1,9 +0,0 @@
-class GreetingCard:
-    def __init__(self, _recipient="Dana Ev", _sender="Eyal Ch"):
-        self._recipient = _recipient
-        self._sender = _sender
-
-    def greeting_msg(self):
-        print(f"The sender is {self._sender}, the recipient is {self._recipient}")
-
-
